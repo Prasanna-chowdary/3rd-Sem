cb1bb5416fa5d4ca26a2de6b8c790ddb5486f0d2
Prasanna-chowdary <sriprasanna2496@gmail.com>
2025-10-15 18:34:05 +0530

    Create Inheritance.cpp

---
+++ Inheritance.cpp
@@ -0,0 +1,41 @@
+ #include<iostream>
+using namespace std;
+void TakeInput(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+/*inheritance*/
+class Person{
+public:
+	string name;
+	int age;
+	void SetName(string word){
+		name = word;
+	}
+	void SetAge(int val){
+		age = val;
+	}
+
+};
+class Student : public Person{
+public:
+	int id;
+	void SetId(int val){
+		id = val;
+	}
+	void Introduce(){
+		cout<<"Hi i am "<<name<<" and my age is "<<age<<" with id as "<<id<<endl;
+	}
+
+};
+
+int main(){
+	TakeInput();
+	Student Prasanna;
+	Prasanna.SetName("Ashok");
+	Prasanna.SetAge(28);
+	Prasanna.SetId(12345);
+	Prasanna.Introduce();
+}
